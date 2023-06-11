46e7ffff0f585f3baea2cc73265ff37b4ba407bf
youatik <112843400+youatik@users.noreply.github.com>
2023-06-11 10:46:42 -0400

    Added the repository fro clients, as wells as a client controller and a client service.

---
+++ src/main/java/repository/ClientRepository.java
@@ -0,0 +1,11 @@
+package repository;
+
+import entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Integer> {
+    // Custom queries or additional methods can be defined here if needed
+}
+
---
+++ src/main/java/services/ClientService.java
@@ -0,0 +1,20 @@
+package services;
+
+import entities.Client;
+import repository.ClientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClientService {
+    private final ClientRepository clientRepository;
+
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public List<Client> getAllClients() {
+        return clientRepository.findAll();
+    }
+}
