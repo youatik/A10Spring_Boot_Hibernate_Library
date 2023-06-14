68f47f383b19b54fffff4a4f62defe49e86ac2f9
youatik <112843400+youatik@users.noreply.github.com>
2023-06-13 20:59:24 -0400

    Added LibraryService class

---
+++ src/main/java/com/example/a10spring_boot_hibernate_library/services/LibraryService.java
@@ -0,0 +1,38 @@
+package com.example.a10spring_boot_hibernate_library.services;
+
+import com.example.a10spring_boot_hibernate_library.entities.Library;
+import com.example.a10spring_boot_hibernate_library.repository.LibraryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class LibraryService {
+
+    private final LibraryRepository libraryRepository;
+
+    @Autowired
+    public LibraryService(LibraryRepository libraryRepository) {
+        this.libraryRepository = libraryRepository;
+    }
+
+    public List<Library> getAllBooks() {
+        return libraryRepository.findAll();
+    }
+
+    public Optional<Library> getBookByEanIsbn13(long eanIsbn13) {
+        return libraryRepository.findById(eanIsbn13);
+    }
+
+    public Library createBook(Library book) {
+        return libraryRepository.save(book);
+    }
+
+    public void updateBook(Library book) {
+        libraryRepository.save(book);
+    }
+}
