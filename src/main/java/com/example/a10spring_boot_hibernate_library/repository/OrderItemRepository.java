8bcecaf6b184161911c3db374e7d22276cbe03a8
youatik <112843400+youatik@users.noreply.github.com>
2023-06-15 18:52:54 -0400

    Added @JsonIdentityInfo in entities

--- src/main/java/com/example/a10spring_boot_hibernate_library/repository/OrderItemRepository.java
+++ src/main/java/com/example/a10spring_boot_hibernate_library/repository/OrderItemRepository.java
@@ -1,12 +1,12 @@
 package com.example.a10spring_boot_hibernate_library.repository;
 
-import com.example.a10spring_boot_hibernate_library.entities.ClientOrder;
+import com.example.a10spring_boot_hibernate_library.entities.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface OrderItemRepository extends JpaRepository<ClientOrder, Integer> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, Integer> {
     // You can add custom query methods here if needed
 }
 
--- src/main/java/com/example/a10spring_boot_hibernate_library/repository/UserAuthenticationRepository.java
+++ src/main/java/com/example/a10spring_boot_hibernate_library/repository/UserAuthenticationRepository.java
@@ -1,12 +1,12 @@
 package com.example.a10spring_boot_hibernate_library.repository;
 
-import com.example.a10spring_boot_hibernate_library.entities.ClientOrder;
+import com.example.a10spring_boot_hibernate_library.entities.UserAuthentication;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface UserAuthenticationRepository extends JpaRepository<ClientOrder, Integer> {
+public interface UserAuthenticationRepository extends JpaRepository<UserAuthentication, Integer> {
     // You can add custom query methods here if needed
 }
 
